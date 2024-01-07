195c23782481ec27cddd0dccfa59032b3d9d152e
ayushjain1306 <122806933+ayushjain1306@users.noreply.github.com>
2024-01-08 00:18:55 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import bodyParser from "body-parser";
 const app = express();
 
 app.use(cors({
-    origin: "",
+    origin: "https://developer-ayush-portfolio.vercel.app",
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
 }));
